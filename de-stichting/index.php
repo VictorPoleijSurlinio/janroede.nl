442e4877a238299698fd96ceacb88ba3ab5e5e07
VictorPoleijSurlinio <victor.poleij@surlinio.com>
2026-06-02 10:31:12 +0200

    stichting styling changes + remove word docs

--- de-stichting/index.php
+++ de-stichting/index.php
@@ -33,8 +33,13 @@ include ABS_PATH . 'inc/navbar.inc.php';
                 <p>Jan Roëde heeft de Jan Roëde Stichting in 2005 opgericht om zijn artistieke nalatenschap op ideële basis te laten beheren als hij er zelf niet meer zou zijn. De Stichting is actief geworden na zijn overlijden in 2007 en heeft sinds 2010 de status van een Algemeen Nut Beogende Instelling (ANBI).</p>
                 <p>De doelstelling van de Jan Roëde Stichting is tweeledig. Allereerst zet zij zich in om de kunstwerken die Jan heeft nagelaten (schilderijen, tekeningen, gouaches, grafiek) zo goed mogelijk te beheren en onder de aandacht te brengen van kunstminnend publiek. Dit gebeurt door tentoonstellingen te organiseren, mee te werken aan publicaties over Jan Roëde, schenkingen te doen aan musea en werk te verkopen aan kunstliefhebbers. De opbrengsten maken het mogelijk te werken aan de tweede doelstelling: het stimuleren van talent in de beeldende kunst, bijvoorbeeld door kunstenaars financieel te ondersteunen. Deze ideële doelstelling wordt breed opgevat. Niet alleen wordt jaarlijks een prijs uitgereikt aan een afstuderend kunstenaar van de Koninklijke Academie van Beeldende Kunst (KABK) in Den Haag, ook steunde Stichting de activiteiten van De Vrolijkheid, een stichting die beeldend kunstenaars inzet om kunstprojecten in asielzoekerscentra te initiëren en te begeleiden.</p>
 
+
+
+            </div>
+
+            <div class="col-lg-6">
                 <!-- BESTUUR EN BASISGEGEVENS -->
-                <h2 id="bestuur-en-basisgegevens" class="mt-5">Bestuur en basisgegevens</h2>
+                <h2 id="bestuur-en-basisgegevens">Bestuur en basisgegevens</h2>
                 <p>Het bestuur van de stichting bestaat uit Louw van Sinderen (voorzitter), Olga van Hulsen, Dick Stapel en Huub van Wersch (secretaris-penningmeester). De bestuursleden verrichten hun werkzaamheden onbetaald.</p>
                 <table class="table table-borderless mt-3">
                     <tbody>
@@ -73,38 +78,32 @@ include ABS_PATH . 'inc/navbar.inc.php';
                     </tbody>
                 </table>
 
-                <!-- BELEIDSPLAN -->
-                <div class="row mt-5 g-4">
-                    <div class="col-lg-6">
-                        <h2 id="beleidsplan">Beleidsplan</h2>
-                        <p>Het beleidsplan van de Jan Roëde Stichting is beschikbaar als download:</p>
-                        <ul class="list-unstyled">
-                            <li class="mb-2">
-                                <a href="<?= STATIC_URL ?>docs/Beleidsplan Jan Roëde Stichting 2026-2029.pdf" target="_blank" class="btn btn-outline-secondary btn-sm me-2"><i class="fa-regular fa-file-pdf me-1"></i> Beleidsplan (PDF)</a>
-                                <a href="<?= STATIC_URL ?>docs/beleidsplan Jan Roëde Stichting 2026-2029.docx" target="_blank" class="btn btn-outline-secondary btn-sm"><i class="fa-regular fa-file-word me-1"></i> Beleidsplan (Word)</a>
-                            </li>
-                        </ul>
-                    </div>
+                </ul>
+            </div>
 
-                    <!-- JAARVERSLAGEN -->
-                    <div class="col-lg-6">
-                        <h2 id="jaarverslagen">Jaarverslagen</h2>
-                        <p>De jaarverslagen zijn beschikbaar als download:</p>
-                        <ul class="list-unstyled">
-                            <li class="mb-2">
-                                <strong>Jaarverslag 2025</strong> &mdash;
-                                <a href="<?= STATIC_URL ?>docs/Jaarverslag en jaarrekening Jan Roede Stichting 2025 (te publiceren op website).pdf" target="_blank" class="btn btn-outline-secondary btn-sm me-2"><i class="fa-regular fa-file-pdf me-1"></i> PDF</a>
-                                <a href="<?= STATIC_URL ?>docs/Jaarverslag en jaarrekening Jan Roede Stichting 2025 (te publiceren op website).docx" target="_blank" class="btn btn-outline-secondary btn-sm"><i class="fa-regular fa-file-word me-1"></i> Word</a>
-                            </li>
-                            <li class="mb-2">
-                                <strong>Jaarverslag 2024</strong> &mdash;
-                                <a href="<?= STATIC_URL ?>docs/Jaarverslag en jaarrekening Jan Roede Stichting 2024 (voor publicatie).pdf" target="_blank" class="btn btn-outline-secondary btn-sm me-2"><i class="fa-regular fa-file-pdf me-1"></i> PDF</a>
-                                <a href="<?= STATIC_URL ?>docs/Jaarverslag en jaarrekening Jan Roede Stichting 2024 (voor publicatie).docx" target="_blank" class="btn btn-outline-secondary btn-sm"><i class="fa-regular fa-file-word me-1"></i> Word</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
+            <!-- BELEIDSPLAN + JAARVERSLAGEN -->
+            <div class="col-lg-6">
+                <h2 id="beleidsplan">Beleidsplan</h2>
+                <p>Het beleidsplan van de Jan Roëde Stichting is beschikbaar als download:</p>
+                <ul class="list-unstyled">
+                    <li class="mb-2">
+                        <a href="<?= STATIC_URL ?>docs/Beleidsplan Jan Roëde Stichting 2026-2029.pdf" target="_blank" class="btn btn-outline-secondary btn-sm"><i class="fa-regular fa-file-pdf me-1"></i> Download Beleidsplan (PDF)</a>
+                    </li>
+                </ul>
+                <h2 id="jaarverslagen">Jaarverslagen</h2>
+                <p>De jaarverslagen zijn beschikbaar als download:</p>
+                <ul class="list-unstyled">
+                    <li class="mb-2">
+                        <strong>Jaarverslag 2025</strong> &mdash;
+                        <a href="<?= STATIC_URL ?>docs/Jaarverslag en jaarrekening Jan Roede Stichting 2025 (te publiceren op website).pdf" target="_blank" class="btn btn-outline-secondary btn-sm"><i class="fa-regular fa-file-pdf me-1"></i> Download PDF</a>
+                    </li>
+                    <li class="mb-2">
+                        <strong>Jaarverslag 2024</strong> &mdash;
+                        <a href="<?= STATIC_URL ?>docs/Jaarverslag en jaarrekening Jan Roede Stichting 2024 (voor publicatie).pdf" target="_blank" class="btn btn-outline-secondary btn-sm"><i class="fa-regular fa-file-pdf me-1"></i> Download PDF</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-12">
                 <!-- ACTIVITEITEN -->
                 <h2 id="activiteiten" class="mt-5">Activiteiten</h2>
 
@@ -122,7 +121,9 @@ include ABS_PATH . 'inc/navbar.inc.php';
                 <p>De Stichting ziet graag dat de kunst die Jan Roëde heeft nagelaten zijn weg vindt naar het kunstminnend publiek. Om die reden zijn wij ook actief met het verkopen van werken uit de collectie. De verkoop verloopt deels via tentoonstellingen en samenwerking met galeries en kunsthandelaars. Daarnaast is het ook mogelijk rechtstreeks via de Stichting in het bezit van een mooi werk van Jan Roëde te komen. Als een of meer op deze website getoonde werken uw belangstelling hebben gewekt, neem dan gerust via de e-mail contact met ons op. Inmiddels hebben op deze manier al aardig wat werken uit onze collectie hun weg gevonden naar liefhebbers. De opbrengsten uit onze verkoop gaan, na aftrek van onze exploitatiekosten, naar de ontwikkeling van kunstzinnig talent, zoals Jan Roëde bij de oprichting heeft beoogd.</p>
                 <a class="btn-client-rounded-purple" href="<?= SITE_URL ?>contact">Contact opnemen</a>
             </div>
+
         </div>
+    </div>
     </div>
 </section>
 
