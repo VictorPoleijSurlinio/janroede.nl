ae58397ea8b501c1dbf9022d53966ab1b0d9e47e
VictorPoleijSurlinio <victor.poleij@surlinio.com>
2026-08-13 10:20:38 +0200

    update popup

--- inc/popup.inc.php
+++ inc/popup.inc.php
@@ -32,13 +32,13 @@ if (!empty($SITE_POPUP_ENABLED)) {
 					<button type="button" class="btn-close primary-color" data-bs-dismiss="modal" aria-label="Sluiten"></button>
 				</div>
 				<div class="modal-body">
-					<h3 class="h4 mb-3">Benefietexpositie voor de Vrolijkheid: Kunst die een glimlach deelt</h3>
-					<p class="mb-3">Van 29 juni t/m 23 augustus organiseert de Jan Ro&euml;de Stichting opnieuw een benefiettentoonstelling voor Stichting de Vrolijkheid, dit keer in de Mesdagzaal van Pulchri Studio.</p>
-					<p class="mb-3">Wij tonen wederom een mooie selectie van schilderijen en werken op papier die voor schappelijke prijzen te koop worden aangeboden. De netto-opbrengsten komen volledig ten goede aan de activiteiten van de Vrolijkheid.</p>
-					<p class="mb-3">De tentoonstelling wordt op 1 augustus om 17.00 uur officieel geopend door Cheeta Bruin, kunsthistoricus, lid van de RvT van de Vrolijkheid en projectleider tentoonstellingen van het Van Gogh Museum.</p>
-					<p class="mb-0">Wij hopen u daar te begroeten, of op een ander moment tijdens de looptijd van de expositie.</p>
+					<h3 class="h4 mb-3">Kunst kopen voor het goede doel: expositie tot 23 augustus, veiling op 21 augustus</h3>
+					<p class="mb-3">Bent u al in Pulchri Studio geweest? Onder de titel <em>Kunst die een glimlach deelt</em> organiseert de Jan Ro&euml;de Stichting een oogstrelende benefietexpositie voor Stichting de Vrolijkheid. In de monumentale Mesdagzaal vindt u nog tot en met 23 augustus een gevarieerde selectie schilderijen en werken op papier van Jan Ro&euml;de.</p>
+					<p class="mb-3">De prijzen beginnen al bij &euro; 50 en de netto-verkoopopbrengst gaat volledig naar de organisatie van kunstzinnige activiteiten en creatieve workshops in azc&rsquo;s.</p>
+					<p class="mb-3">Daarnaast zal Venduehuis op 21 augustus om 15:30 uur circa 20 grote schilderijen van Ro&euml;de veilen in de Mesdag. Ook de opbrengst hiervan gaat volledig naar De Vrolijkheid. Met uw aankoop brengt u kleur in het leven van jonge vluchtelingen. Kom kijken en draag een steentje bij aan deze benefietactie.</p>
+					<p class="mb-0">Meer informatie over de benefietveiling vindt u <a href="https://vrolijkheid.nl/ti/specials/benefietveiling-jan-roede/" target="_blank" rel="noopener noreferrer">hier</a>.</p>
 				</div>
-				<div class="modal-footer d-flex justify-content-between">
+				<div class="modal-footer d-flex justify-content-between gap-2 flex-wrap">
 					<a class="btn btn-client-rounded-purple" href="https://www.pulchri.nl/nl/tentoonstellingen/benefietexpositie-jan-roede-de-vrolijkheid-kunst-die-een-glimlach-deelt-1/" target="_blank" rel="noopener noreferrer">Naar Pulchri Studio</a>
 					<button type="button" class="btn btn-client-rounded" data-bs-dismiss="modal" aria-label="Sluit popup">Sluiten</button>
 				</div>
